bf5966e52ea37f04a7978aa16fffcf42a1f60f69
Roland Farkas <hello@rolandfarkas.com>
2022-05-10 17:39:01 +0100

    Update ampanalyticssupport-plugin-insert.php

--- inc/ampanalyticssupport-plugin-insert.php
+++ inc/ampanalyticssupport-plugin-insert.php
@@ -45,14 +45,13 @@ class ampanalyticssupportInsert
 
               return $content;
               add_filter('the_content', 'add_post_content');
-          }
+        
           
         
           
       } 
     }
          
-   
-
+  }
 
 }
