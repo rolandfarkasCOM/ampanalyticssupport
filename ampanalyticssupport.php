b8b0d9b33f1f80af14824d299f5d21761197d4d5
Roland Farkas <hello@rolandfarkas.com>
2022-05-10 20:10:11 +0100

    Update ampanalyticssupport.php

--- ampanalyticssupport.php
+++ ampanalyticssupport.php
@@ -82,7 +82,7 @@ if ( !class_exists( 'ampanalyticssupportPlugin' ) ) {
 
 	$ampanalyticssupportPlugin = new ampanalyticssupportPlugin();
 	$ampanalyticssupportPlugin->register();
-   ;
+   
 	add_action('the_content',  $ampanalyticssupportPlugin->insert(), 9999);
 
 	// activation
