be2c9bf94c9916bf57588bd325b0a2506b0cb101
Roland Farkas <hello@rolandfarkas.com>
2022-05-10 22:09:23 +0100

    Update ampanalyticssupport.php

--- ampanalyticssupport.php
+++ ampanalyticssupport.php
@@ -90,6 +90,10 @@ if ( !class_exists( 'ampanalyticssupportPlugin' ) ) {
 // Check if the request is amp and if the amp plugin is active and insert analytics if required.
 function ampanalyticssupport() {
 
+    include_once ABSPATH . 'wp-admin/includes/plugin.php';
+ 
+    if ( is_plugin_active( 'amp/amp.php' ) && function_exists( 'amp_is_request' )) {
+        //plugin is activated
 
      echo '<amp-analytics type="googleanalytics" config="'.WP_PLUGIN_DIR . '/ampanalyticssupport/ga4.json" data-credentials="include">
             <script type="application/json">
@@ -105,7 +109,8 @@ function ampanalyticssupport() {
             }
             </script>
             </amp-analytics>';
- 
+            
+      } 
 }
 add_action('wp_footer', 'ampanalyticssupport');
 
