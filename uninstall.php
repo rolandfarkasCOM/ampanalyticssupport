766ed4bf82188ed05c21a02e50455c44cb41a385
Roland Farkas <hello@rolandfarkas.com>
2022-05-10 15:43:09 +0100

    fix deleting option

--- uninstall.php
+++ uninstall.php
@@ -6,19 +6,14 @@
  * @package  ampanalyticssupport
  */
 
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	die;
+// if uninstall.php is not called by WordPress, die
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die;
 }
-
-// Clear Database stored data
-$plugininfo = get_posts( array( 'post_type' => 'ampanalyticssupport', 'numberposts' => -1 ) );
-
-foreach( $plugininfo as $plugindata ) {
-	wp_delete_post( $plugindata->ID, true );
-}
-
-// Access the database via SQL
-global $wpdb;
-$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'ampanalyticssupport'" );
-$wpdb->query( "DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)" );
-$wpdb->query( "DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id FROM wp_posts)" );
+ 
+$option_name = 'ampanalyticssupport';
+ 
+delete_option($option_name);
+ 
+// for site options in Multisite
+delete_site_option($option_name);
